e8f0803ae5cf42b10fcc9b264a0694f1de61f4c6
aar001 <aar001@gmkzoloto.ru>
2026-02-17 21:53:57 +0700

    Created and filled cart service file, changed cart routers

--- routers/cart.py
+++ routers/cart.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from uuid import UUID
 from sqlalchemy.orm import joinedload
+from service.cart import CartService
 
 router = APIRouter(
     prefix="/cart",
@@ -14,68 +15,31 @@ router = APIRouter(
 
 @router.get("/", response_model=list[CartRead], status_code=200)
 async def get_all_carts(session: AsyncSession = Depends(get_db)):
-    result = await session.scalars(
-        select(Cart)
-        .options(joinedload(Cart.item))
-    )
-    carts = result.all()
-
+    carts = await CartService.get_all_carts(session)
     if not carts:
-        raise HTTPException(status_code=404)
-
+        raise HTTPException(status_code=404, detail="carts not found")
     return carts
 
 @router.get("/{user_id}", response_model=list[CartRead], status_code=200)
 async def get_user_cart(user_id: UUID, session: AsyncSession = Depends(get_db)):
-    cart_items = await session.scalars(
-        select(Cart)
-        .options(joinedload(Cart.item))
-        .where(user_id == Cart.user_id)
-    )
-
-    all_cart_items = cart_items.all()
-
-    if not all_cart_items:
-        raise HTTPException(status_code=404, detail="User dont have items in cart or cart doesnt exists.")
-
-    return all_cart_items
+    cart = await CartService.get_cart_by_id(user_id, session)
+    if cart is None:
+        raise HTTPException(status_code=404, detail="cart not found")
 
 @router.post("/", response_model=CartRead, status_code=201)
-async def add_to_cart(
+async def create_cart(
     cart_in: CartCreate, 
     session: AsyncSession = Depends(get_db)
 ):
-    cart = await session.scalar(
-        select(Cart)
-        .options(joinedload(Cart.item))
-        .where(
-            Cart.user_id == cart_in.user_id, 
-            Cart.item_id == cart_in.item_id
-        )
-    )
-    data_to_put = cart_in.model_dump(exclude_defaults=True)
-
-    if cart is None:
-        cart = Cart(**data_to_put)
-        session.add(cart)
-    else:
-        cart.items_count += 1
-
-    await session.commit()
-    await session.refresh(cart)
-
+    cart = await CartService.create_cart(cart_in, session)
     return cart
+
 
 @router.delete("/{cart_id}", status_code=204)
 async def delete_cart_by_id(cart_id: UUID, session: AsyncSession = Depends(get_db)):
-    cart = await session.get(Cart, cart_id)
-    
-    if cart is None:
-        raise HTTPException(status_code=404)
-    else:
-        session.delete(cart)
-
-    await session.commit()
+    result = await CartService.delete_cart_by_id(cart_id, session)
+    if not result:
+        raise HTTPException(status_code=404, detail="cart not found")
 
 @router.patch("/{cart_id}", status_code=200)
 async def edit_item_count_in_cart(
@@ -83,28 +47,10 @@ async def edit_item_count_in_cart(
     cart_in: CartBase,
     session: AsyncSession = Depends(get_db)
 ):
-    async with session.begin():
-        cart = await session.scalar(
-            select(Cart)
-            .options(joinedload(Cart.item))
-            .where(Cart.id == cart_id)
-        )
-
-        if cart is None:
-            raise HTTPException(status_code=404)
-        
-        if cart_in.items_count == 0 or cart.items_count == 0:
-            await session.delete(cart)
-            return {"status": "item deleted from the cart"}
-        
-        data_to_update = cart_in.model_dump(exclude_unset=True)
-
-        for key, value in data_to_update.items():
-            setattr(cart, key, value)
-
-    await session.refresh(cart)
-    return {
-        "items_count": cart.items_count,
-        "id": cart.id,
-        "item": cart.item
-    }
+    cart_to_upd = await CartService.delete_cart_by_id(cart_id, cart_in, session)
+    
+    if cart_to_upd is None:
+        raise HTTPException(status_code=404, detail="cart not found")
+    if cart_to_upd == "deleted":
+        return {"status": "cart was deleted success"}
+    return cart_to_upd
---
+++ service/cart.py
@@ -0,0 +1,93 @@
+from models import Cart
+from schemas import CartBase, CartCreate, CartRead
+
+from sqlalchemy.orm import joinedload
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from uuid import UUID
+
+class CartService:
+
+    @staticmethod
+    async def get_all_carts(session: AsyncSession):
+        carts = await session.scalars(select(Cart))
+        carts_data = carts.all()
+
+        return carts_data
+    
+    @staticmethod
+    async def get_cart_by_id(user_id: UUID, session: AsyncSession):
+        return await session.scalar(
+            select(Cart)
+            .where(Cart.id == user_id)
+        )
+
+    @staticmethod
+    async def create_cart(cart_in: CartCreate, session: AsyncSession):
+        cart = await session.scalar(
+            select(Cart)
+            .options(joinedload(Cart.item))
+            .where(
+                Cart.item_id == cart_in.item_id,
+                Cart.user_id == cart_in.user_id
+            )
+        )
+
+        cart_model = cart_in.model_dump()
+        
+        if cart is None:
+            cart = Cart(**cart_model)
+            session.add(cart)
+        else:
+            cart.items_count += cart_in.items_count
+
+        await session.commit()
+
+        cart = await session.scalar(
+            select(Cart)
+            .options(joinedload(Cart.item))
+            .where(
+                Cart.item_id == cart_in.item_id,
+                Cart.user_id == cart_in.user_id
+            )
+        )
+        await session.refresh(cart)
+
+        return cart
+    
+    @staticmethod
+    async def delete_cart_by_id(cart_id: UUID, session: AsyncSession):
+        cart_to_del = await session.get(Cart, cart_id)
+
+        if not cart_to_del:
+            return False
+        
+        session.delete(cart_to_del)
+        await session.commit()
+
+        return True
+    
+    @staticmethod
+    async def update_item_count_by_id(cart_id: UUID, cart_in: CartBase, session: AsyncSession):
+        status = False
+        async with session.begin():
+            cart_to_upd = await session.scalar(
+                select(Cart)
+                .options(joinedload(Cart.item))
+                .where(Cart.id == cart_id)
+            )
+            if cart_to_upd is None:
+                return None
+
+            if cart_in.items_count == 0 or cart_to_upd.items_count <= 0:
+                session.delete(cart_to_upd)
+                return "deleted"
+
+            cart_model = cart_in.model_dump(exclude_unset=True)
+
+            for key, value in cart_model.items():
+                setattr(cart_to_upd, key, value)
+
+        await session.refresh(cart_to_upd)
+        return cart_to_upd
